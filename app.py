be4c7b4b75fffc8754e20f4b6f5f282fef2833ca
debianvowt <ericksilvasales1@gmail.com>
2025-05-24 17:20:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
-from synthgpt.cerebro import responder_pergunta, get_memoria
+from synthgpt.cerebro import responder_pergunta
 
 app = Flask(__name__)
 CORS(app)  # libera cors para desenvolvimento local
